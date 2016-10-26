0263390f70639b725d0c8d643dbfd3abd445fce9
Mark Trompell <Mark.Trompell@ulb.hhu.de>
2016-10-26 13:00:04 +0200

    stream statt paged_each für mysql2. mysql2 lädt mit offset strategy immer alle Datensätze bis zum offset + limit ein und schmeisst alles bis auf limit weg. stream sollte da performanter sein.

--- common.rb
+++ common.rb
@@ -23,3 +23,38 @@ def enable_debug()
   end
   binding.pry if have_pry && ENV['DEBUG']=="1"
 end
+
+
+class DBReader
+
+  def initialize(database_connection_or_url, dataset_proc, options = {})
+      @rows_per_fetch = options.delete(:rows_per_fetch) || 1000
+      puts @rows_per_fetch
+
+      @db = if database_connection_or_url.is_a?(String)
+        Sequel.connect(database_connection_or_url, options)
+      else
+        database_connection_or_url
+      end
+      @adapter = @db.adapter_scheme
+      @dataset = dataset_proc.call(@db)
+  end
+
+  def each(&block)
+    return enum_for(__method__) unless block_given?
+
+    if @adapter==:mysql2 
+      # mysql2 ignoriert strategy= filter offset wird ab 500000 Datensätzen unerträglich.
+      @dataset.stream.each do |row|
+        yield(row)
+      end
+    else
+      @dataset.paged_each(:rows_per_fetch=>@rows_per_fetch) do |row|
+        yield(row)
+      end
+    end
+    
+    self
+  end
+
+end
